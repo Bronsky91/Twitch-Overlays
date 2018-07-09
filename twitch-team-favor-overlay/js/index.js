65aa8d05fc468ececbfc4b5a12d48e6d0aa36f66
Bryan Reed <bryan87reed@gmail.com>
2018-07-08 17:57:45 -0700

    Updates interval loop to catch the gameid each time checking game status

--- twitch-team-favor-overlay/js/index.js
+++ twitch-team-favor-overlay/js/index.js
@@ -1,15 +1,15 @@
-axios.get('http://localhost:4567/api/v1/gameid')
-  .then(function (response) {
-    game_info = response.data;
-    setInterval(function () {
+setInterval(function () {
+  axios.get('http://localhost:4567/api/v1/gameid')
+    .then(function (response) {
+      game_info = response.data;
       blueWidth = 0;
       axios.get('http://localhost:4567/api/v1/teamvotes/' + game_info.game_id)
         .then(function (teamresponse) {
           teamvotes = teamresponse.data;
           b_tally = [];
           r_tally = [];
-          for (i=0;i<teamvotes.length;i++){
-            if (teamvotes[i].vote == game_info.blue_team){
+          for (i = 0; i < teamvotes.length; i++) {
+            if (teamvotes[i].vote == game_info.blue_team) {
               b_tally.push(teamvotes[i]);
             } else if (teamvotes[i].vote == game_info.red_team) {
               r_tally.push(teamvotes[i]);
@@ -21,7 +21,11 @@ axios.get('http://localhost:4567/api/v1/gameid')
           if (percent_favor == Infinity) {
             percent_favor = 100
           }
-          blueWidth = document.getElementById('blue').style.width = percent_favor * 100 + '%'
+          if (game_info.game_started == true) {
+            blueWidth = document.getElementById('blue').style.width = percent_favor * 100 + '%'
+          } else {
+            blueWidth = document.getElementById('blue').style.width = '100%'
+          }
         })
-    }, 3000)
-})
+    })
+}, 3000)
